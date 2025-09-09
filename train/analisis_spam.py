6ac5f1e93a73ca043dd03dd2f8e98cde509c5bc3
Mari1345Espu <mafeescobar3110@gmail.com>
2025-09-09 17:05:38 -0500

    Limpieza: borro archivos no usados y agrego cambios nuevos

--- train/analisis_spam.py
+++ train/analisis_spam.py
@@ -11,10 +11,10 @@ import numpy as np
 try:
     df = pd.read_csv(r'C:\Users\mafee\Clasificador de correos\data\spam_dataset(1).csv')
 except FileNotFoundError:
-    print("Error: The file 'spam_dataset (1).csv' was not found.")
+    print("Error: No encuentra el archivo dataset.")
     exit()
 
-# Seleccionar las variables predictoras (features) y la variable objetivo (label)
+# Seleccionar los features que usaremos
 features = ['suspicious_terms_count', 'subject_length', 'link_count', 'auth_pass_spf_dkim_dmarc']
 X = df[features]
 y = df['label']
@@ -46,7 +46,7 @@ for i in range(num_executions):
     print(f"F1-Score: {f1:.4f}")
     
     # Guardar el modelo y las métricas en el historial
-    model_filename = f'logistic_regression_model_run_{i+1}.joblib'
+    model_filename = f'logistic_regression_model_ejecutado{i+1}.joblib'
     joblib.dump(model, model_filename)
     
     history.append({
@@ -71,10 +71,10 @@ plt.ylabel('Puntuación de la Métrica')
 plt.ylim(0.8, 1.0)
 plt.legend()
 plt.grid(True)
-plt.savefig('metrics_history_plot.png')
+plt.savefig('Historico_metricas.png')
 plt.close()
 
-print("Gráfico del historial de métricas generado y guardado como 'metrics_history_plot.png'.")
+print("Gráfico del historial de métricas generado y guardado como 'Historico_metricas.png'.")
 
 # --- Gráfica de la línea de regresión de la ÚLTIMA ÉPOCA ---
 # Se utiliza el modelo y los datos de la última ejecución (i = num_executions - 1)
@@ -105,7 +105,7 @@ plt.xlabel('Longitud del Asunto (subject_length)')
 plt.ylabel('Probabilidad de ser SPAM')
 plt.ylim(-0.05, 1.05)
 plt.legend()
-plt.savefig('final_sigmoid_predictions_plot.png')
+plt.savefig('Prediccion_final.png')
 plt.close()
 
-print("Gráfico de la línea de regresión final generado y guardado como 'final_sigmoid_predictions_plot.png'.")
+print("Gráfico de la línea de regresión final generado y guardado como 'Prediccion_final.png'.")
